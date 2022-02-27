c3dc072175780f0569c5c9b69302fe0c0f41e9c6
jjrx <jjohal52@gmail.com>
2022-02-26 23:23:20 -0800

    installed selenium to get dynamic table content

--- scraper.py
+++ scraper.py
@@ -1,17 +1,22 @@
-import requests
 import time
 import os
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
 
 
 def scrape_canvas(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
+    driver = webdriver.Chrome()
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    driver.quit()
     table = soup.find(id="syllabus")
+    rows = table.find_all('tr')
     print(table)
 
 
-rows = scrape_canvas(
-    'https://oregonstate.instructure.com/courses/1877222/assignments/syllabus')
+CS361_URL = 'https://oregonstate.instructure.com/courses/1877222/assignments/syllabus'
+CS372_URL = 'https://oregonstate.instructure.com/courses/1830291/assignments/syllabus'
+rows = scrape_canvas(CS361_URL)
 print(rows)
