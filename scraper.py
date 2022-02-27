d19a0f9a46e7199baf9183aa9f99adfb4376692d
jjrx <jjohal52@gmail.com>
2022-02-26 23:46:51 -0800

    processed scraped content into df

--- scraper.py
+++ scraper.py
@@ -12,11 +12,51 @@ def scrape_canvas(url):
     soup = BeautifulSoup(html, "html.parser")
     driver.quit()
     table = soup.find(id="syllabus")
+
+    # strip out extraneous trailing info, screenreader content, and calendar icons
+    for div in table.find_all("div", {'class': 'special_date_title'}):
+        div.decompose()
+    for span in table.find_all("span", {'class': 'screenreader-only'}):
+        span.decompose()
+    for icon in table.find_all("i", {'class': 'icon-assignment'}):
+        icon.decompose()
+
+    last_date = None
+    dates = []
+    tasks = []
+    times = []
+
     rows = table.find_all('tr')
-    print(table)
+    for row in rows:
+        cells = row.find_all('td')
+
+        # some rows only have 2 columns if multiple assignments are due on a given day
+        if len(cells) == 3:
+            td_count = 1
+        elif len(cells) == 2:
+            td_count = 2
+            dates.append(last_date)
+
+        for cell in cells:
+            cur_text = cell.text.strip().replace('\n', '')
+            # date column (contains due date)
+            if td_count == 1:
+                dates.append(cur_text)
+                last_date = cur_text
+            # details column (contains task name)
+            elif td_count == 2:
+                tasks.append(cur_text)
+            # due column (contains due by time)
+            elif td_count == 3:
+                times.append(cur_text)
+            td_count += 1
+
+    syllabus_df = pd.DataFrame(
+        {'Tasks': tasks, 'Dates': dates, 'Times': times})
+    print(syllabus_df)
 
 
-CS361_URL = 'https://oregonstate.instructure.com/courses/1877222/assignments/syllabus'
-CS372_URL = 'https://oregonstate.instructure.com/courses/1830291/assignments/syllabus'
-rows = scrape_canvas(CS361_URL)
-print(rows)
+if __name__ == "__main__":
+    CS361_URL = 'https://oregonstate.instructure.com/courses/1877222/assignments/syllabus'
+    CS372_URL = 'https://oregonstate.instructure.com/courses/1830291/assignments/syllabus'
+    rows = scrape_canvas(CS361_URL)
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from scraper import scrape_canvas
+import json
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def hello_world():
     CS361_URL = 'https://oregonstate.instructure.com/courses/1877222/assignments/syllabus'
     # CS372_URL = 'https://oregonstate.instructure.com/courses/1830291/assignments/syllabus'
     syllabus = scrape_canvas(CS361_URL)
-    return syllabus
+    stringified_syllabus = json.dumps(syllabus)
+    return stringified_syllabus
