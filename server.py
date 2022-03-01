4c6af034bf516577aa62fed71e00cdf299bdb3f4
jjrx <jjohal52@gmail.com>
2022-03-01 01:33:20 -0800

    implemented html templates, intermediate download step, and naive cache to store scraped content

--- server.py
+++ server.py
@@ -1,23 +1,46 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request
 from scraper import get_syllabus_content, convert_syllabus_to_df, process_df_for_todoist
-import json
 
 app = Flask(__name__)
 
+courses = {}
+last_processed = None
+
 
 @app.route("/")
-def hello_world():
-    return '<p>CS 361 ID: 1877222</p><p>CS 372 ID: 1830291</p>'
+def home():
+    return render_template('index.html')
 
 
-@app.route("/todoist/<int:class_id>")
-def todoist(class_id):
-    URL = f'https://oregonstate.instructure.com/courses/{class_id}/assignments/syllabus'
-    syllabus = get_syllabus_content(URL)
-    df = convert_syllabus_to_df(syllabus)
-    todoist_df = process_df_for_todoist(df)
+@app.route("/", methods=['POST'])
+def todoist():
+    if request.method == 'POST':
+        course_url = request.form['course_url']
+        tms = request.form['tms']
+
+        if course_url not in courses:
+            courses[course_url] = {}
+            syllabus = get_syllabus_content(course_url)
+            df = convert_syllabus_to_df(syllabus)
+            courses[course_url]['raw'] = df
+
+        if tms not in courses[course_url]:
+            if tms == 'Todoist':
+                tms_df = process_df_for_todoist(df)
+            elif tms == 'Asana':
+                pass
+            courses[course_url][tms] = tms_df
+
+        global last_processed
+        last_processed = courses[course_url][tms]
+
+        return render_template('download.html', tms=tms, course_url=course_url)
+
+
+@app.route("/download")
+def download():
     return Response(
-        todoist_df.to_csv(),
+        last_processed.to_csv(),
         mimetype="text/csv",
         headers={"Content-disposition":
                  "attachment; filename=syllabus_for_todoist.csv"})
